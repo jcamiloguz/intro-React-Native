8ed91438681e8ca605d33569712aac25840128dc
jcamiloguz <jcamiloguz1@gmail.com>
2022-01-28 01:45:42 -0500

    fixing a bug

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { NavigationStack } from './src/navigations/NavigationStack'
-import { NavigationStack } from './src/navigations/NavigationStack'
 
 export default function App() {
   return (
