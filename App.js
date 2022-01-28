72acccae7684ef50524d35144b2d7c9b241689a7
jcamiloguz <jcamiloguz1@gmail.com>
2022-01-28 01:45:08 -0500

    adding a bug

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { NavigationStack } from './src/navigations/NavigationStack'
+import { NavigationStack } from './src/navigations/NavigationStack'
 
 export default function App() {
   return (
